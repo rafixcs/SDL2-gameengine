216a5b2b5c4507fe950235794308ff11e53e9712
Rafael Silva <rafaelsilva@localhost.localdomain>
2021-02-25 14:48:50 -0300

    fedora and ubuntu requirements

---
+++ requirements-fedora.sh
@@ -0,0 +1,5 @@
+sudo dnf install SDL2-devel.x86_64 -y
+sudo dnf install SDL2_image-devel.x86_64 -y
+sudo dnf install SDL2_ttf-devel.x86_64 -y
+sudo dnf install SDL2_mixer-devel.x86_64 -y
+#sudo dnf install liblua5.3-dev
---
+++ requirements-ubuntu.sh
@@ -0,0 +1,5 @@
+sudo apt install libsdl2-dev
+sudo apt install libsdl2-image-dev
+sudo apt install libsdl2-ttf-dev
+sudo apt install libsdl2-mixer-dev
+sudo apt install liblua5.3-dev
